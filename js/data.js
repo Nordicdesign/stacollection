04dfbad8ba7d5f11981f82cfa1669e2ee837e85e
Alex Lillo <alex.lillo@gmail.com>
2018-09-18 07:21:41 +0100

    json clean up on unneeded data

--- js/data.js
+++ js/data.js
@@ -1,136 +1,85 @@
 const collectionCalendar = [
   {
-    "day": 3,
-    "month": 9,
-    "year": 2018,
     "weeknumber": 36,
     "type": "green",
     "bank holiday": false
   },
  {
-   "day": 10,
-   "month": 9,
-   "year": 2018,
    "weeknumber": 37,
    "type": "landfill",
    "bank holiday": false
  },
  {
-   "day": 17,
-   "month": 9,
-   "year": 2018,
    "weeknumber": 38,
    "type": "green",
    "bank holiday": false
  },
  {
-   "day": 24,
-   "month": 9,
-   "year": 2018,
    "weeknumber": 39,
    "type": "landfill",
    "bank holiday": false
  },
  {
-   "day": 1,
-   "month": 10,
-   "year": 2018,
    "weeknumber": 40,
    "type": "green",
    "bank holiday": false
  },
  {
-   "day": 8,
-   "month": 10,
-   "year": 2018,
    "weeknumber": 41,
    "type": "landfill",
    "bank holiday": false
  },
  {
-   "day": 15,
-   "month": 10,
-   "year": 2018,
    "weeknumber": 42,
    "type": "green",
    "bank holiday": false
  },
  {
-   "day": 22,
-   "month": 10,
-   "year": 2018,
    "weeknumber": 43,
    "type": "landfill",
    "bank holiday": false
  },
  {
-   "day": 29,
-   "month": 10,
-   "year": 2018,
    "weeknumber": 44,
    "type": "green",
    "bank holiday": false
  },
  {
-   "day": 5,
-   "month": 11,
-   "year": 2018,
    "weeknumber": 45,
    "type": "landfill",
    "bank holiday": false
  },
  {
-   "day": 12,
-   "month": 11,
-   "year": 2018,
    "weeknumber": 46,
    "type": "green",
    "bank holiday": false
  },
  {
-   "day": 19,
-   "month": 11,
-   "year": 2018,
    "weeknumber": 47,
    "type": "landfill",
    "bank holiday": false
  },
  {
-   "day": 26,
-   "month": 11,
-   "year": 2018,
    "weeknumber": 48,
    "type": "green",
    "bank holiday": false
  },
  {
-   "day": 3,
-   "month": 12,
-   "year": 2018,
    "weeknumber": 49,
    "type": "landfill",
    "bank holiday": false
  },
  {
-   "day": 10,
-   "month": 12,
-   "year": 2018,
    "weeknumber": 50,
    "type": "green",
    "bank holiday": false
  },
  {
-   "day": 17,
-   "month": 12,
-   "year": 2018,
    "weeknumber": 51,
    "type": "landfill",
    "bank holiday": false
  },
  {
-   "day": 24,
-   "month": 12,
-   "year": 2018,
    "weeknumber": 52,
    "type": "green",
    "bank holiday": false
